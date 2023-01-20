3aa55479ca8fb7ea13bf971a9041252f343cd48f
Masood Ahmed <masood.ahmed.dev@gmail.com>
2023-01-20 22:42:57 +0500

    Added search bar

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import OrdersList from "../views/OrdersList.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,15 +12,7 @@ const router = createRouter({
     {
       path: "/orders",
       name: "orders",
-      component: OrdersList,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/OrdersList.vue"),
     },
   ],
 });
--- frontend/src/views/OrdersList.vue
+++ frontend/src/views/OrdersList.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
+  <el-row>
 
-  </div>
+    <el-col :span="4">
+      <div class="customHeader">
+        <faUserSecret></faUserSecret>
+        <div class="searchLabel">
+            <Search width="40" height="40" />
+            <label>Search</label>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="20">
+      <el-input>
+        
+      </el-input>
+    </el-col>
+  </el-row>
 </template>
 
-<script>
+<script lang="ts">
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 export default {
+  components: { faUserSecret },
 
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 
+.searchLabel {
+  font-family: 'Helvetica Neue';
+  font-weight: 600;
+}
+
+.searchLabel {
+  label {
+    font-size: 2em;
+    font-weight: bold;
+  }
+}
 </style>
